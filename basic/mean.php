f9ace3afdecc5fb6fdeb55b652525dcf5ccec768
sk8792300-del <sk8792300@gmail.com>
2026-08-23 23:31:30 +0530

    php basic

---
+++ basic/mean.php
@@ -0,0 +1,39 @@
+<?php
+// $a=10;
+// $b=10;
+// $c=10;
+// $d=10;
+// $e=10;
+//  $sum=$a+$b+$c+$d+$e;
+//  $mean=$sum/5;
+//  echo $mean;
+// var_dump($mean);
+
+// $name = readline("what's your name?");
+
+// echo "Gracias $name\n";
+// echo 'Gracias $name\n';
+// echo 'Gracias'.$name;
+
+// $x = "1000";
+// $y = 5;
+
+// echo ($x*$y);
+
+// echo"Welcome to my computer quiz!";
+
+// $playing = strtoupper(readline("Do you want to play?"));
+// if($playing != "yes"){
+//     exit;
+// }
+   
+
+// echo"Okay! Let's play :";
+// $score = 0;
+$age = readline("what's your age?");
+if($age>=18){
+    echo"yor are eligible to vote :,$age";
+}
+else{
+    echo " you are not eligible to vote";
+}
