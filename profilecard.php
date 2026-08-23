a9c3a7cd0eb58081dceba652979de75b290c2a90
sk8792300-del <sk8792300@gmail.com>
2026-08-23 22:24:35 +0530

    update file

---
+++ profilecard.php
@@ -0,0 +1,20 @@
+<?php
+  // .......... PROFILE CARD...........
+  $name=strtolower(readline("enter a name\n"));
+  $age=strtolower(readline("enter a age\n"));
+  $course=strtolower(readline("enter a course\n"));
+  $enrollement=strtolower(readline("enrollement(true/false)\n"));
+  
+   // double coated
+  echo"Name:$name\n";
+  echo"Age:$age\n";
+  echo"Course:$course\n";
+  echo"Enrollement:$enrollement\n";
+
+  // single coated 
+  echo'NAME:$name';
+  echo'Age:$age';
+  echo'Course:$course';
+  echo'Enrollement:$enrollement';
+
+
